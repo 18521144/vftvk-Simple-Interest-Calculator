2fd74b421d7a350fdde93370649aa685fd4f9c12
Nguyen Huu Nghia <nhn230gmail.com>
2021-07-24 05:00:49 +0000

    Update 2

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
+
 function compute()
 {
+    var p = document.getElementById("principal")
     var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    if(principal<=0){
+        alert("Enter a postitive number");
+        p.focus();
+    }
+    else{
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate /100;
+        var year = new Date().getFullYear()+parseInt(years)-1;
+        document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%.</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\><br\>"
+
+    }
 }
 
 function updateRate()
